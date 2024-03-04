c21c131a9e5d42c64456af678f4ddb30461fe311
Githuba Jeremy <githuba9520@gmail.com>
2024-03-04 16:13:00 +0300

    final commit

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 /**
 Challenge: 
 
-1. Fetch a random activity from the Bored API
-url: https://apis.scrimba.com/bored/api/activity
-
-2. Display the text of the activity in the browser
+- Make the styling more exciting once an activity idea comes 
+back from the Bored API
+    - Resources: DOM element "classList" property, uigradients.com, 
+      Google Fonts, color.adobe.com
+    - Some ideas:
+      - Change the title from "BoredBot" to something more exciting!
+      - Change the background to something less drab.
+      - Bonus: Animate something on the screen to move around and add more 
+        excitement to the page
 */
 
-const activityContainer = document.getElementById("activity-container");
-
-fetch("https://apis.scrimba.com/bored/api/activity")
-  .then((res) => res.json())
-  .then((data) => (activityContainer.innerHTML = `<p> ${data.activity} </p>`));
+document.getElementById("get-activity").addEventListener("click", () => {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("activity").textContent = data.activity;
+      document.getElementById("title").textContent = "🦾 HappyBot🦿";
+      document.body.classList.add("fun");
+    });
+});
